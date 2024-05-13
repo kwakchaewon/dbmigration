d3030346f254adf00d7381a3386cd77e69964b36
kwakchaewon <cwkwak@edentns.com>
2024-05-13 13:10:36 +0900

    Querydsl 설정과 검증

--- src/test/java/com/example/dbmigration/DbmigrationApplicationTests.java
+++ src/test/java/com/example/dbmigration/DbmigrationApplicationTests.java
@@ -1,13 +1,40 @@
 package com.example.dbmigration;
 
+import com.example.dbmigration.entity.Hello;
+import com.example.dbmigration.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.swing.text.html.parser.Entity;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class DbmigrationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
+    }
 
 }
